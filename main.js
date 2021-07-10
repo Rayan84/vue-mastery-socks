5a85efa22a8cf1d6290b000d384c96a314c7a36b
Rayan84 <rayan_84@live.com>
2021-07-10 17:20:41 +0200

    Add border to img

--- main.js
+++ main.js
@@ -2,7 +2,10 @@ var product = "Socks"
 var app = new Vue ({
     el: '#app',
     data: {
+        altText: "A pair of socks",
         product: "Socks",
-        image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg'
+        image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+        inventory: 0,
+        details: ["80% cotton", "20% polyester", "Gender-neutral"]
     }
 })
