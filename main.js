070fe696b1abffe9e80f6ab89c889e1de42772ab
Rayan84 <rayan_84@live.com>
2021-07-11 13:50:37 +0200

    Add event listeners and functions

--- main.js
+++ main.js
@@ -4,8 +4,28 @@ var app = new Vue ({
     data: {
         altText: "A pair of socks",
         product: "Socks",
-        image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
         inventory: 0,
-        details: ["80% cotton", "20% polyester", "Gender-neutral"]
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green',
+                variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+            },
+            {
+                variantId: '2235',
+                variantColor: 'blue',
+                variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg'
+            }
+        ],
+        methods: {
+            addToCart() {
+                this.cart += 1
+            },
+            updateProduct = function (variantImage) {
+                this.image = variantImage
+            }
+        }
+    
     }
 })
