d3f2abcf7335ae25ee359a859be6c4910738d588
Rayan84 <rayan_84@live.com>
2021-07-13 00:16:01 +0200

    Remove debug code

--- main.js
+++ main.js
@@ -1,34 +1,32 @@
-var product = "Socks"
-var app = new Vue ({
-    el: '#app',
-    data: {
-        altText: "A pair of socks",
-        product: "Socks",
-        inventory: 0,
-        details: ["80% cotton", "20% polyester", "Gender-neutral"],
-        image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
-        variants: [
-            {
-                variantId: 2234,
-                variantColor: 'green',
-                variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
-            },
-            {
-                variantId: '2235',
-                variantColor: 'blue',
-                variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg'
-            }
-        ],
-        cart: 0,
-        methods: {
-            addToCart() {
-                this.cart += 1,
-                alert ('add to cart clicked')
-            },
-            updateProduct (link) {
-                this.image = link
-            }
-        }
+//Create a new button and method to decrement the value of `cart`.
 
+var app = new Vue({
+  el: '#app',
+  data: {
+    product: 'Socks',
+    image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+    inStock: true,
+    details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: 'green',
+        variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg'
+      },
+      {
+        variantId: 2235,
+        variantColor: 'blue',
+        variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg'
+      }
+    ],
+    cart: 0
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1
+    },
+    updateProduct(variantImage) {
+      this.image = variantImage
     }
+  }
 })
