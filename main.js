e0f6c97ce8150672e8ebf873e1778efa10de895e
Rayan84 <rayan_84@live.com>
2021-08-29 22:13:16 +0200

    Add template content to the review form

--- main.js
+++ main.js
@@ -118,6 +118,20 @@ Vue.component('product', {
         <textarea id="review"></textarea>
       </p>
 
+      <p>
+        <label for="rating">Rating:</label>
+        <select id="rating">
+          <option>5</option>
+          <option>4</option>
+          <option>3</option>
+          <option>2</option>
+          <option>1</option>
+        </select>
+      </p>
+
+      <p>
+        <input type="submit" value="Submit">
+      </p>
     </form
     `,
     data() {
