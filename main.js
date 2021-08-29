187ff8bb56d0445b72abd38337875b7fa9ddcd80
Rayan84 <rayan_84@live.com>
2021-08-29 22:31:09 +0200

    Add on submit functionality

--- main.js
+++ main.js
@@ -107,7 +107,7 @@ Vue.component('product', {
 
   Vue.component('product-review', {
     template: `
-    <form class="review-form">
+    <form class="review-form" @submit.prevent="onSubmit">
       <p>
         <label for="name">Name:</label>
         <input id="name" v-model="name">
@@ -115,12 +115,12 @@ Vue.component('product', {
 
       <p>
         <label for="review">Review:</label>
-        <textarea id="review"></textarea>
+        <textarea id="review" v-model="review"></textarea>
       </p>
 
       <p>
         <label for="rating">Rating:</label>
-        <select id="rating">
+        <select id="rating" v-model.number="rating">
           <option>5</option>
           <option>4</option>
           <option>3</option>
@@ -132,7 +132,7 @@ Vue.component('product', {
       <p>
         <input type="submit" value="Submit">
       </p>
-    </form
+    </form>
     `,
     data() {
       return {
@@ -140,6 +140,17 @@ Vue.component('product', {
         review: null,
         rating: null,
       }
+    },
+    methods: {
+      onSubmit() {
+        let productReview = {
+        name: this.name,
+        review: this.review,
+        rating: this.rating
+      }
+      this.name = null
+      this.review = null 
+      this.rating = null
     }
   })
 
